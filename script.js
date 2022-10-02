bba298cb662cd490f4fa66590ad41a717b2fd617
Gabriela P <gabriela.poryazova@gmail.com>
2022-10-02 13:08:45 +0300

    Create and display book cards on submitting the form

--- script.js
+++ script.js
@@ -32,12 +32,40 @@ formElement.addEventListener('submit', (e)=>{
     
     
 })
-//add book to library and create a book card 
+//create and display book cards 
+const bodyContainer = document.querySelector('.body-contaner');
+console.log(bodyContainer)
+function createBookElements(bookObject){
+        const bookCard = document.createElement('div');
+        bookCard.setAttribute('class', 'book-card')
+        const titleDiv = document.createElement('div');
+        const authDiv = document.createElement('div');
+        const pageDiv = document.createElement('div');
+        const removeBtn = document.createElement('button');
+        const readBtn = document.createElement('button');
+        bookCard.append(titleDiv, authDiv, pageDiv, removeBtn, readBtn);
+        bookCard.setAttribute('data-id', bookObject.id);
+        titleDiv.textContent = bookObject.title;
+        authDiv.textContent = bookObject.author
+        pageDiv.textContent = bookObject.pages;
+        removeBtn.textContent = "Remove book";
+        if (bookObject.read == "true") {
+            readBtn.textContent = "Read"
+        }else{
+            readBtn.textContent = "Not Read"
+        }
+        bodyContainer.appendChild(bookCard);
+}
+
+//add book to library 
 function addBookToLibrary(bookObject){
     formElement.reset();
     popupScreen.setAttribute('style',"display: none");
     myLibrary.push(bookObject);
+    createBookElements(bookObject);
 }
+
+
 
 
 
